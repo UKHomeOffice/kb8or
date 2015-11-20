5d3c5c930fa441ff7c5d2dd209e5332ede6f2851
Lewis Marshall <lewis@technoplusit.co.uk>
2015-11-20 17:32:35 +0000

    Added secrets from files

--- libs/kb8_utils.rb
+++ libs/kb8_utils.rb
@@ -5,10 +5,20 @@ require_relative 'kb8_run'
 class Kb8Utils
   def self.load_yaml(file_path)
     begin
-      data = YAML.load(File.read(file_path))
-    rescue Exception => e
+      data = YAML.load(Kb8Utils.load(file_path))
+    rescue Exception
       # do some logging
       raise $!, "Error parsing YAML file: #{file_path}: #{$!}", $!.backtrace
+    end
+    data
+  end
+
+  def self.load(file_path)
+    begin
+      data = File.read(file_path)
+    rescue Exception
+      # do some logging
+      raise $!, "Error reading file: #{file_path}: #{$!}", $!.backtrace
     end
     data
   end
