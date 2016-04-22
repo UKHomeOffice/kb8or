2cb9e757bb6e92a673085583a80900d1dfa994da
Lewis Marshall <lewis@technoplusit.co.uk>
2016-04-22 16:06:54 +0100

    Latest kubectl

--- bin/downloads.sh
+++ bin/downloads.sh
@@ -8,14 +8,14 @@ function download() {
     MD5SUM=$3
 
     until [[ -x ${OUTPUT_FILE} ]] && [[ $(md5sum ${OUTPUT_FILE} | cut -f1 -d' ') == ${MD5SUM} ]]; do
-      wget -O ${OUTPUT_FILE} ${URL}
+      wget -nv -O ${OUTPUT_FILE} ${URL}
       chmod +x ${OUTPUT_FILE}
     done
 }
 
-download https://github.com/UKHomeOffice/kubernetes/releases/download/v1.2.0-kubectl/kubectl-linux-amd64 \
+download https://storage.googleapis.com/kubernetes-release/release/v1.2.2/bin/linux/amd64/kubectl \
          /usr/local/bin/kubectl \
-         a1b7cda8223c8c06221ab1c602875236
+         e5203a62738b6815c34dd1855ba0d7dd
 
 download https://github.com/UKHomeOffice/s3secrets/releases/download/0.1.2/s3secrets_0.1.2_linux_x86_64 \
          /usr/local/bin/s3secrets \
